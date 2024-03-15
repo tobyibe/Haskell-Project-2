8181e795d023d0fd7748454f5419d94eb876287c
Toby Ibe <tci1@st-andrews.ac.uk>
2024-03-15 16:21:26 +0000

    evaluation functionality

--- Expr.hs
+++ Expr.hs
@@ -8,7 +8,7 @@ type Name = String
 -- values. You will need to add other operations, and variables
 data Expr = Add Expr Expr
           | ToString Expr
-          | Val Int
+          | Val Value
   deriving Show
 
 -- These are the REPL commands
@@ -16,12 +16,26 @@ data Command = Set Name Expr -- assign an expression to a variable name
              | Print Expr    -- evaluate an expression and print the result
   deriving Show
 
+data Value = IntVal Int | StrVal String
+  deriving Show
+
+maybeAdd :: Maybe Value -> Maybe Value -> Maybe Value
+maybeAdd (Just (IntVal x)) (Just (IntVal y)) = Just (IntVal (x + y))
+
+maybeValueToInt :: Maybe Value -> Int
+maybeValueToInt (Just (IntVal x)) = x
+
+nameToValue:: [(Name, Int)] -> Name -> Maybe Value
+nameToValue vars name = foldr(\x acc -> if fst x == name then Just (IntVal (snd x)) else acc) Nothing vars
+
 eval :: [(Name, Int)] -> -- Variable name to value mapping
         Expr -> -- Expression to evaluate
-        Maybe Int -- Result (if no errors such as missing variables)
-eval vars (Val x) = Just x -- for values, just give the value directly
-eval vars (Add x y) = Nothing -- return an error (because it's not implemented yet!)
-eval vars (ToString x) = Nothing
+        Maybe Value -- Result (if no errors such as missing variables)
+
+eval vars (Val (StrVal x)) = nameToValue vars x -- for StrVal values, string is variable name, replace it with value 
+eval vars (Val (IntVal x)) = Just (IntVal x) -- For IntVal values, just give the value directly
+eval vars (Add x y) = maybeAdd (eval vars x) (eval vars y) -- return an error (because it's not implemented yet!)
+eval vars (ToString x) = Just (StrVal (show (maybeValueToInt (eval vars x))))
 
 digitToInt :: Char -> Int
 digitToInt x = fromEnum x - fromEnum '0'
@@ -48,7 +62,7 @@ pExpr = do t <- pTerm
 
 pFactor :: Parser Expr
 pFactor = do d <- digit
-             return (Val (digitToInt d))
+             return (Val (IntVal (digitToInt d)))
            ||| do v <- letter
                   error "Variables not yet implemented" 
                 ||| do char '('
